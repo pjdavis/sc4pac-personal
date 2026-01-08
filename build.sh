0948cc6e3bab30ba36323c0d8acefdc465505074
PJ Davis <pj.davis@gmail.com>
2026-01-08 00:15:34 -0500

    need tools

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ LABEL="PJ'S SC4PAC CHANNEL"
 
 $(which sc4pac) channel build --label "$LABEL" --metadata-source-url https://github.com/pjdavis/sc4pac-personal/blob/channel/yaml --output .gh_pages/channel channel/yaml
 
-# cp -p ../sc4pac--tools/web/target/website/channel/* .gh_pages/channel
+cp -p ../sc4pac-tools/web/target/website/channel/* .gh_pages/channel
 # cp -pr ../sc4pac/docs/. .gh_pages
